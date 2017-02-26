54e596ec4649cddba1a1cf45f4db35e51e652d23
Sek Cheong <scheong@gmail.com>
2017-02-25 22:00:44 -0600

    :cat2:

--- src/ml/learner/nerualnet/NeuralNet.java
+++ src/ml/learner/nerualnet/NeuralNet.java
@@ -31,25 +31,26 @@ public class NeuralNet {
 	public double[][][] W() {
 		return _W;
 	}
-	
-	
+
+
 	public Layer addLayer(int units) {
 		int index = _addLayers.size();
 		Layer layer;
-		
-		if (index==0) {
+
+		if (index == 0) {
 			layer = new Layer(units, units);
 		}
 		else {
-			//number of intput units is the number of unites of the previous layer
-			layer = new Layer(units, _addLayers.get(index-1).units());
+			// number of intput units is the number of unites of the previous
+			// layer
+			layer = new Layer(units, _addLayers.get(index - 1).units());
 		}
-		
+
 		layer.index(_addLayers.size());
 		layer.net(this);
 		_addLayers.add(layer);
 
-		return layer;		
+		return layer;
 	}
 
 
@@ -81,64 +82,63 @@ public class NeuralNet {
 		W[2][1] = new double[] { -0.7, 0.6, 0.2, 0.7 };
 		W[2][2] = new double[] { -0.3, 0.7, 0.2, 0.8 };
 
-		W[3] = new double[1][3];		
+		W[3] = new double[1][3];
 		W[3][0] = new double[] { 0.1, 0.8, 0.5 };
-		
-		Instance[] ex = new Instance[1];		
+
+		Instance[] ex = new Instance[1];
 		ex[0] = new Instance();
-		
+
 		ex[0].features = new double[] { 0.3, 0.7 };
 		ex[0].target = new double[] { 0.9 };
-		
-//		Instance[] ex = new Instance[4];
-//		
-//		ex[0] = new Instance();
-//		ex[0].features = new double[] { 0.2, 0.9, 0.4 };
-//		ex[0].target = new double[] { 0.7, 0.3 };
-//
-//		ex[1] = new Instance();
-//		ex[1].features = new double[] { 0.1, 0.3, 0.5 };
-//		ex[1].target = new double[] { 0.6, 0.4 };
-//
-//		ex[2] = new Instance();
-//		ex[2].features = new double[] { 0.9, 0.7, 0.8 };
-//		ex[2].target = new double[] { 0.9, 0.5 };
-//
-//		ex[3] = new Instance();
-//		ex[3].features = new double[] { 0.6, 0.4, 0.3 };
-//		ex[3].target = new double[] { 0.3, 0.4 };
-		
+
+		// Instance[] ex = new Instance[4];
+		//
+		// ex[0] = new Instance();
+		// ex[0].features = new double[] { 0.2, 0.9, 0.4 };
+		// ex[0].target = new double[] { 0.7, 0.3 };
+		//
+		// ex[1] = new Instance();
+		// ex[1].features = new double[] { 0.1, 0.3, 0.5 };
+		// ex[1].target = new double[] { 0.6, 0.4 };
+		//
+		// ex[2] = new Instance();
+		// ex[2].features = new double[] { 0.9, 0.7, 0.8 };
+		// ex[2].target = new double[] { 0.9, 0.5 };
+		//
+		// ex[3] = new Instance();
+		// ex[3].features = new double[] { 0.6, 0.4, 0.3 };
+		// ex[3].target = new double[] { 0.3, 0.4 };
+
 		return ex;
 	}
 
-	
+
 	private void initialize(double[][][] W, double[][] I, double[][] Y) {
 		Layer layers[] = this.layers();
 
 		// initializes the weights for each hidden
 		for (int n = 1; n < W.length; n++) {
 
-			W[n] = new double[layers[n].units() ][];
+			W[n] = new double[layers[n].units()][];
 
 			// units in the previous unit plus a bias unit
 			I[n] = new double[layers[n - 1].units()];
-			Y[n] = new double[layers[n].units()];
-			
+			Y[n] = new double[layers[n - 1].units()];
+
 			// number of units connecting from n - 1
 			for (int j = 0; j < layers[n].units(); j++) {
 				W[n][j] = new double[layers[n - 1].units()];
 			}
-			
+
 			layers[n].weightInitializer().initializeWeights(W[n]);
 		}
 
-		for (int n = 1; n < layers.length; n++) {	
+		for (int n = 1; n < layers.length; n++) {
 			String str = "W[" + n + "]\n" + Format.matrix(W[n]) + "\n";
 			Trace.log(str);
 		}
 	}
 
-	
 
 	public void train(Instance[] examples, double eta, double precision, int maxEpoch) {
 		// the current mean squared error
@@ -150,7 +150,7 @@ public class NeuralNet {
 
 		// p = number of examples
 		int p = examples.length;
-		
+
 		Layer layers[] = this.layers();
 
 		// W[n][j][i] are weight matrices whose elements denote the value of
@@ -177,15 +177,14 @@ public class NeuralNet {
 
 		this.initialize(W, I, Y);
 		this._W = W;
-		
-				
+
 		examples = debugNetValues(W);
 		X = new double[examples[0].features.length + 1];
-		
+
 		Trace.log("W=");
 		Trace.log(Format.matrix(W));
 		Trace.log("X=[" + Format.matrix(X) + "]");
-				
+
 		while (true) {
 
 			prevMSE = currMSE;
@@ -216,18 +215,18 @@ public class NeuralNet {
 	}
 
 
-	private void computeForward(Layer layers[], double[][][] W, double[][] I, double[][] Y, double X[]) {							 
-		for (int n = 1; n < W.length; n++) {					
-			Function g  = layers[n].activationFunction();			
-			computeIY(W, X, I, Y, g,  n);			
+	private void computeForward(Layer layers[], double[][][] W, double[][] I, double[][] Y, double X[]) {
+		for (int n = 1; n < W.length; n++) {
+			Function g = layers[n].activationFunction();
+			computeIY(W, X, I, Y, g, n);
 		}
 	}
-	
-	
-	private void computeIY(double[][][] W, double[] X, double[][] I, double [][]Y, Function g,  int n) {
-		
+
+
+	private void computeIY(double[][][] W, double[] X, double[][] I, double[][] Y, Function g, int n) {
+
 		Trace.log("W[" + n + "] = [" + Format.matrix(W[n]) + "]");
-		
+
 		if (n == 1) {
 			for (int j = 0; j < W[n].length; j++) {
 				double z = 0;
@@ -235,12 +234,14 @@ public class NeuralNet {
 					z = z + W[n][j][i] * X[i];
 				}
 				I[n][j] = z;
-				Y[n][j + 1] = g.compute(I[n][j]);
+				Y[n][j] = g.compute(I[n][j]);
 			}
 			Y[n][0] = BIAS;
 		}
-		else if (n < W.length - 1) {
+		else if (n < W.length - 1) {								
 			for (int j = 0; j < W[n].length; j++) {
+				Trace.log("W[" + n + "][" + j +"] = [" + Format.matrix(W[n][j]) + "]");
+				Trace.log("Y[" + (n - 1) + "] = [" + Format.matrix(Y[n - 1]) + "]");
 				double z = 0;
 				for (int i = 0; i < W[n][j].length; i++) {
 					z = z + W[n][j][i] * Y[n - 1][j];
@@ -260,7 +261,7 @@ public class NeuralNet {
 				Y[n][j] = g.compute(I[n][j]);
 			}
 		}
-		
+
 		Trace.log("I[" + n + "] = [" + Format.matrix(I[n]) + "]");
 		Trace.log("Y[" + n + "] = [" + Format.matrix(Y[n]) + "]");
 
